98bc5dab9695500018846caf69fe190f3015618d
ManuelS05 <manuel05118579@gmail.com>
2025-04-02 19:40:13 -0600

    Add Orbitask/app/src/main/java/com/example/orbitask/ui/theme/Color.kt 1743644413287

---
+++ Orbitask/app/src/main/java/com/example/orbitask/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.orbitask.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light theme colors
+val LightPrimary = Color(0xFF006D40)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightPrimaryContainer = Color(0xFF8CF7BC)
+val LightOnPrimaryContainer = Color(0xFF002110)
+
+// Dark theme colors
+val DarkPrimary = Color(0xFF70DA9C)
+val DarkOnPrimary = Color(0xFF003920)
+val DarkPrimaryContainer = Color(0xFF005231)
+val DarkOnPrimaryContainer = Color(0xFF8CF7BC)
+
+// Task priority colors
+val HighPriority = Color(0xFFFF6B6B)
+val MediumPriority = Color(0xFFFFD166)
+val LowPriority = Color(0xFF06D6A0)
